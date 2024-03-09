3b4dd0427e500de7a4ebd660be20e09c730d9929
Christopher Pedraza <gabalixreborn@gmail.com>
2024-03-09 17:12:49 -0600

    Agregar tarjeta con micrófono

--- DREAM Lab Frontend/src/App.jsx
+++ DREAM Lab Frontend/src/App.jsx
@@ -1,26 +1,38 @@
-import './App.css'
-import ImageSlider2 from './Home/ImageSlider2'
-import ImageSlider1 from './Home/ImageSlider'
+// import ImageSlider from './Home/ImageSlider'
+import SpeechBotCard from './Home/SpeechBotCard'
+import './output.css'
 
-const OPTIONS = { dragFree: true, loop: true }
-const SLIDE_COUNT = 5
-const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
+// const OPTIONS = { dragFree: true, loop: true }
+// const SLIDE_COUNT = 5
+// const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
 
-const unsplash_prefix = 'https://images.unsplash.com/photo-'
-const unsplash_suffix = '?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
-const IMAGES = [`${unsplash_prefix}1524781289445-ddf8f5695861${unsplash_suffix}`,
-`${unsplash_prefix}1610194352361-4c81a6a8967e${unsplash_suffix}`,
-`${unsplash_prefix}1618202133208-2907bebba9e1${unsplash_suffix}`,
-`${unsplash_prefix}1548021682-1720ed403a5b${unsplash_suffix}`,
-`${unsplash_prefix}1496753480864-3e588e0269b3${unsplash_suffix}`,
-`${unsplash_prefix}1613346945084-35cccc812dd5${unsplash_suffix}`,
-`${unsplash_prefix}1516681100942-77d8e7f9dd97${unsplash_suffix}`,]
+// const unsplash_prefix = 'https://images.unsplash.com/photo-'
+// const unsplash_suffix = '?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+// const IMAGES = [`${unsplash_prefix}1524781289445-ddf8f5695861${unsplash_suffix}`,
+// `${unsplash_prefix}1610194352361-4c81a6a8967e${unsplash_suffix}`,
+// `${unsplash_prefix}1618202133208-2907bebba9e1${unsplash_suffix}`,
+// `${unsplash_prefix}1548021682-1720ed403a5b${unsplash_suffix}`,
+// `${unsplash_prefix}1496753480864-3e588e0269b3${unsplash_suffix}`,
+// `${unsplash_prefix}1613346945084-35cccc812dd5${unsplash_suffix}`,
+// `${unsplash_prefix}1516681100942-77d8e7f9dd97${unsplash_suffix}`,]
 
 function App() {
   return (
     <>
-      <ImageSlider1 images={IMAGES} options={OPTIONS} />
-      <ImageSlider1 images={IMAGES} options={OPTIONS} />
+      {/* <ImageSlider images={IMAGES} options={OPTIONS} /> */}
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '2rem',
+      }}>
+        <div style={{
+          width: '80%',
+          height: '18rem',
+        }}>
+          <SpeechBotCard />
+        </div>
+      </div>
     </>
   )
 }
---
+++ DREAM Lab Frontend/src/Home/SpeechBotCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { EvervaultCard, Icon } from "../components/ui/evervault-card.jsx";
+
+function SpeechBotCard() {
+	return (
+		<div className="border border-black/[0.2] dark:border-white/[0.2] flex flex-col items-start mx-auto p-4 relative h-full w-full">
+			<Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
+			<Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
+			<Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
+			<Icon className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black" />
+			<h1 style={{ color: 'white' }}>Hola</h1>
+			<EvervaultCard />
+		</div>
+	);
+}
+
+export default SpeechBotCard;
