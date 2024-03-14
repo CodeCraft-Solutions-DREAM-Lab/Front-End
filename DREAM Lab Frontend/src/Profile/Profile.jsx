65b0acae1b95829d948cc632705bba9ea211624e
JaimedevSwirvle <jaime@swirvle.com>
2024-03-14 13:40:28 -0600

    Ya se borra una reservación desde perfil

--- DREAM Lab Frontend/src/Profile/Profile.jsx
+++ DREAM Lab Frontend/src/Profile/Profile.jsx
@@ -17,42 +17,11 @@ function Profile() {
 
 	}, []);
 
-	// const reservaciones = [{
-	// 	"idReservacion": 1,
-	// 	"idUsuario": "A0XXXXXX1",
-	// 	"idSala": 1,
-	// 	"idExperiencia": 1,
-	// 	"horaInicio": "1970-01-01T10:00:00.000Z",
-	// 	"duracion": 2.5,
-	// 	"fecha": "2024-01-01T00:00:00.000Z",
-	// 	"numMesa": 2
-	// },
-	// {
-	// 	"idReservacion": 1,
-	// 	"idUsuario": "A0XXXXXX1",
-	// 	"idSala": 1,
-	// 	"idExperiencia": 1,
-	// 	"horaInicio": "1970-01-01T10:00:00.000Z",
-	// 	"duracion": 2.5,
-	// 	"fecha": "2024-01-01T00:00:00.000Z",
-	// 	"numMesa": 2
-	// },
-	// {
-	// 	"idReservacion": 1,
-	// 	"idUsuario": "A0XXXXXX1",
-	// 	"idSala": 1,
-	// 	"idExperiencia": 1,
-	// 	"horaInicio": "1970-01-01T10:00:00.000Z",
-	// 	"duracion": 2.5,
-	// 	"fecha": "2024-01-01T00:00:00.000Z",
-	// 	"numMesa": 2
-	// },];
-
 	return (
 		<div>
 			<BotonRegresar />
 			<h1>Profile</h1>
-			<ListaReservaciones reservations={reservaciones} />
+			<ListaReservaciones reservations={reservaciones} setReservations={setReservaciones}/>
 		</div>
 	);
 }
