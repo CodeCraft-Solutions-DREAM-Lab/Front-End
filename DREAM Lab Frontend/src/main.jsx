36f27da0fa10c895edda6f2c6666ba4c098af009
Christopher Pedraza <gabalixreborn@gmail.com>
2024-03-13 23:12:42 -0600

    Agregar librerías para tener date pickers usando mui x

--- DREAM Lab Frontend/src/main.jsx
+++ DREAM Lab Frontend/src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { NextUIProvider } from "@nextui-org/react";
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <NextUIProvider>
-    <App />
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <App />
+    </LocalizationProvider>
   </NextUIProvider>
 )
