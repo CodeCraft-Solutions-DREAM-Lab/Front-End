64e571848c8b8793785a89a9df1178bef22eb0d0
Christopher Pedraza <gabalixreborn@gmail.com>
2024-04-06 09:30:36 -0600

    Eliminar context del landing page

--- DREAM Lab Frontend/src/LandingPage/LandingPage.jsx
+++ DREAM Lab Frontend/src/LandingPage/LandingPage.jsx
@@ -1,15 +1,9 @@
-import { useContext } from "react";
-import { UserContext } from "../Global/Context/UserContext";
-
 function LandingPage() {
-  const { user } = useContext(UserContext);
-
   return (
     <>
       <h1>Landing Page</h1>
       <p>
         <strong>User: </strong>
-        {user}
       </p>
     </>
   );
