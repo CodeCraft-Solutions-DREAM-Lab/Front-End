78455ac9cc60eab87bbad78e452d9ff5578e7921
Christopher Pedraza <gabalixreborn@gmail.com>
2024-04-06 22:08:56 -0600

    Agregar comentarios auth

--- DREAM Lab Frontend/src/Global/Auth.js
+++ DREAM Lab Frontend/src/Global/Auth.js
@@ -1,39 +1,25 @@
-import { redirect } from "react-router-dom";
 import { postData } from "./Database";
 
-export async function requireAuth() {
-  // TODO: Cambiar que en vez de un valor booleano, haya una tabla que relacione
-  // a los usuarios y contraseña encriptada, con un token. De esta manera,
-  // cuando se haga login, se valida que el usuario y contraseña existan y sean
-  // válidos, y se regresa el token para el local storage. Posteriormente,
-  // cuando se quiera entrar a una página protegida, se valida que ese token
-  // exista.
-  // El problema de que sea booleano, es que en los developer tools puedes tú
-  // poner ese valor como true. En cambio, es imposible que sepas la cadena de
-  // caracteres de un usuario para poder entrar.
-  const isLoggedIn = localStorage.getItem("loggedin");
-
-  if (!isLoggedIn) {
-    redirect("/login");
-  }
-
-  return null;
-}
-
+// Función para validar las credenciales del usuario y redirigir a la página
 export async function loginAction({ formData }) {
   const user = formData.get("user");
   const password = formData.get("password");
+  // Llamar a la API para validar las credenciales
   return postData("http://localhost:3000/authUsuario", {
     usuario: user,
     contrasena: password,
   })
     .then((response) => {
+      // De la API se recibe un jwt (JSON Web Token)
       const jwt = response.jwt;
+      // Guardar el token y el usuario en el local storage
       localStorage.setItem("token", jwt);
       localStorage.setItem("user", user);
+      // Regresar la dirección a la que se debe redirigir
       return "/home";
     })
     .catch(() => {
+      // Si las credenciales no son válidas, regresar a la página de login
       return "/login";
     });
 }
