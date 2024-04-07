2a78cc2b3e5697cba6b035d18430b001242758c0
JaimedevSwirvle <jaime@swirvle.com>
2024-04-06 18:18:22 -0600

    Cambios hasta ahora

---
+++ DREAM Lab Frontend/src/Reservaciones/SelectorEquipo.jsx
@@ -0,0 +1,11 @@
+
+function SelectorEquipo(props) {
+    return (
+        <div>
+            <h1>Selector de Equipo</h1>
+            <p>Este es el selector de equipo.</p>
+        </div>
+    );
+}
+
+export default SelectorEquipo;
