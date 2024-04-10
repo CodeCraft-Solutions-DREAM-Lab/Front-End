4723b9aeaa6696e9531795723fb7bf87b1b28662
Jinellie <89491839+Jinellie@users.noreply.github.com>
2024-04-09 18:29:43 -0600

    Feat: (Home) Agregué los gets para traer la información de las experiencias, falta probar

--- DREAM Lab Frontend/src/Home/HomePage.jsx
+++ DREAM Lab Frontend/src/Home/HomePage.jsx
@@ -7,42 +7,41 @@ import Navbar from "../components/general/NavBar.jsx"; // Import the Navbar comp
 import "./HomePage.css";
 import axios from "axios";
 
-
-const OPTIONS = { dragFree: true, loop: true, startIndex: 0};
+const OPTIONS = { dragFree: true, loop: true, startIndex: 0 };
 
 const unsplash_prefix = "https://images.unsplash.com/photo-";
 const unsplash_suffix =
   "?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80";
 
-  const IMAGES = [
-    { id: "1", url: "/ImagenSalaVR.png", title: "Sala VR" },
-    { id: "2", url: "/ImagenUsoRouters.png", title: "Deep Net" },
-    { id: "3", url: "/ImagenExpGoogle.png", title: "Testing Land" },
-    {
-      id: "4",
-      url: `${unsplash_prefix}1496753480864-3e588e0269b3${unsplash_suffix}`,
-      title: "Electric Garage",
-    },
-    {
-      id: "5",
-      url: `${unsplash_prefix}1613346945084-35cccc812dd5${unsplash_suffix}`,
-      title: "Electric Garage",
-    },
-    {
-      id: "6",
-      url: `${unsplash_prefix}1516681100942-77d8e7f9dd97${unsplash_suffix}`,
-      title: "Electric Garage",
-    },
-    {
-      id: "7",
-      url: `${unsplash_prefix}1709777114364-f1d4da772786${unsplash_suffix}`,
-      title: "Electric Garage",
-    },
-    { id: "8", url: "/ImagenCursoSwift.png", title: "Curso de Swift" },
-    { id: "9", url: "/ImagenConnections.png", title: "Deep Net" },
-    { id: "10", url: "/ImagenCursoSwift.png", title: "Curso de Swift" },
-    { id: "11", url: "/ImagenConnections.png", title: "Deep Net" },
-  ];
+const IMAGES = [
+  { id: "1", url: "/ImagenSalaVR.png", title: "Sala VR" },
+  { id: "2", url: "/ImagenUsoRouters.png", title: "Deep Net" },
+  { id: "3", url: "/ImagenExpGoogle.png", title: "Testing Land" },
+  {
+    id: "4",
+    url: `${unsplash_prefix}1496753480864-3e588e0269b3${unsplash_suffix}`,
+    title: "Electric Garage",
+  },
+  {
+    id: "5",
+    url: `${unsplash_prefix}1613346945084-35cccc812dd5${unsplash_suffix}`,
+    title: "Electric Garage",
+  },
+  {
+    id: "6",
+    url: `${unsplash_prefix}1516681100942-77d8e7f9dd97${unsplash_suffix}`,
+    title: "Electric Garage",
+  },
+  {
+    id: "7",
+    url: `${unsplash_prefix}1709777114364-f1d4da772786${unsplash_suffix}`,
+    title: "Electric Garage",
+  },
+  { id: "8", url: "/ImagenCursoSwift.png", title: "Curso de Swift" },
+  { id: "9", url: "/ImagenConnections.png", title: "Deep Net" },
+  { id: "10", url: "/ImagenCursoSwift.png", title: "Curso de Swift" },
+  { id: "11", url: "/ImagenConnections.png", title: "Deep Net" },
+];
 
 const initialData = [
   {
@@ -72,13 +71,39 @@ function HomePage() {
   const [prevScrollPos, setPrevScrollPos] = useState(0);
   const [visible, setVisible] = useState(true);
   const [experiences, setExperiences] = useState([]);
+  const [ufs, setUfs] = useState([]);
+  const [salas, setSalas] = useState([]);
 
   useEffect(() => {
-    axios.get("/experiencias")
-      .then(response => {
+    axios
+      .get("/salas")
+      .then((response) => {
+        setSalas(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching salas:", error);
+      });
+  }, []);
+
+  useEffect(() => {
+    // Hacer la solicitud HTTP para obtener la información de las UFs
+    axios
+      .get("/experiencias/UFs")
+      .then((response) => {
+        setUfs(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching UFs:", error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get("/experiencias/autodirigidas")
+      .then((response) => {
         setExperiences(response.data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Error fetching experiences:", error);
       });
   }, []);
@@ -103,12 +128,11 @@ function HomePage() {
       setVisible(prevScrollPos > currentScrollPos || currentScrollPos === 0);
       setPrevScrollPos(currentScrollPos);
     };
-  
+
     window.addEventListener("scroll", handleScroll);
-  
+
     return () => window.removeEventListener("scroll", handleScroll);
   }, [prevScrollPos]);
-  
 
   const handleProcessedText = (processedText) => {
     setProcessedTranscript(processedText);
@@ -118,7 +142,7 @@ function HomePage() {
   return (
     <>
       <Navbar visible={visible} /> {/* Use the Navbar component */}
-       <div className="background-container">
+      <div className="background-container">
         <div className="home-background-image-container">
           <div className="left-blobs-container">
             <img
@@ -140,8 +164,6 @@ function HomePage() {
         </div>
       </div>
       <div className="page-content">
-        
-
         <SpeechBotCard height="25rem" onProcessedText={handleProcessedText} />
 
         {showRecommendations && (
@@ -151,26 +173,46 @@ function HomePage() {
           />
         )}
 
-
         <div className="carousel-container">
           <h1>RECOMENDACIONES</h1>
-          <ImageSlider images={IMAGES} titles={IMAGES.map((item) => item.title)} options={OPTIONS} />
+          <ImageSlider
+            images={IMAGES}
+            titles={IMAGES.map((item) => item.title)}
+            options={OPTIONS}
+          />
         </div>
         <div className="carousel-container">
-        <h1>SALAS</h1>
-          <ImageSlider images={IMAGES} options={OPTIONS} />
+          <h1>SALAS</h1>
+          <ImageSlider
+            images={salas.map((sala) => ({
+              id: sala.idSala,
+              url: sala.fotoURL,
+              title: sala.nombre,
+            }))}
+            options={OPTIONS}
+          />
         </div>
         <div className="carousel-container">
-        <h1>PRÁCTICAS AUTODIRIGIDAS</h1>
-          <ImageSlider images={experiences.map(experience => ({
-                id: experience.idExperiencia,
-                url: experience.portadaURL,
-                title: experience.nombre
-              }))} options={OPTIONS} />
+          <h1>PRÁCTICAS AUTODIRIGIDAS</h1>
+          <ImageSlider
+            images={experiences.map((experience) => ({
+              id: experience.idExperiencia,
+              url: experience.portadaURL,
+              title: experience.nombre,
+            }))}
+            options={OPTIONS}
+          />
         </div>
         <div className="carousel-container">
-        <h1>UNIDADES DE FORMACIÓN</h1>
-          <ImageSlider images={IMAGES} options={OPTIONS} />
+          <h1>UNIDADES DE FORMACIÓN</h1>
+          <ImageSlider
+            images={ufs.map((uf) => ({
+              id: uf.idExperiencia,
+              url: uf.portadaURL,
+              title: uf.nombre,
+            }))}
+            options={OPTIONS}
+          />
         </div>
       </div>
     </>
