65ef3341cc1ad2a42ada12f5c641eb36a0700226
Jinellie <89491839+Jinellie@users.noreply.github.com>
2024-04-10 23:36:33 -0600

    Update HomePage.jsx

--- DREAM Lab Frontend/src/Home/HomePage.jsx
+++ DREAM Lab Frontend/src/Home/HomePage.jsx
@@ -83,6 +83,18 @@ function HomePage() {
   const defaultURL = "http://localhost:3000";
 
   useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      setVisible(prevScrollPos > currentScrollPos || currentScrollPos === 0);
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [prevScrollPos]);
+
+  useEffect(() => {
     const fetchData = async () => {
       setIsLoadingSalas(true);
       setErrorSalas(null);
@@ -94,7 +106,6 @@ function HomePage() {
         if (typeof data === "string") {
           setSalas(JSON.parse(data));
         } else {
-          console.log(salas)
           setSalas(data); // Assuming data is already an object
         }
       } catch (error) {
@@ -114,13 +125,14 @@ function HomePage() {
       setErrorExperiences(null);
 
       try {
-        const response = await axios.get(defaultURL + "/experiencias/autodirigidas");
+        const response = await axios.get(
+          defaultURL + "/experiencias/autodirigidas"
+        );
         const data = response.data;
 
         if (typeof data === "string") {
           setExperiences(JSON.parse(data));
         } else {
-          console.log(experiences)
           setExperiences(data); // Assuming data is already an object
         }
       } catch (error) {
@@ -140,20 +152,20 @@ function HomePage() {
       setErrorUfs(null);
 
       const userID = getFromLocalStorage("user");
-      console.log("este es el user: " + userID);
+     
+      
       try {
         const response = await axios.get(defaultURL + "/experiencias/UFs", {
           params: {
-            user: userID
-          }
+            user: userID,
+          },
         });
         const data = response.data;
-        console.log(data);
+        
 
         if (typeof data === "string") {
           setUfs(JSON.parse(data));
         } else {
-          console.log(ufs)
           setUfs(data); // Assuming data is already an object
         }
       } catch (error) {
@@ -181,23 +193,10 @@ function HomePage() {
     }
   }, [processedTranscript]);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollPos = window.pageYOffset;
-      setVisible(prevScrollPos > currentScrollPos || currentScrollPos === 0);
-      setPrevScrollPos(currentScrollPos);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [prevScrollPos]);
-
   const handleProcessedText = (processedText) => {
     setProcessedTranscript(processedText);
   };
 
-  //console.log("Processed Transcript in HomePage:", processedTranscript);
   return (
     <>
       <Navbar visible={visible} /> {/* Use the Navbar component */}
@@ -241,51 +240,49 @@ function HomePage() {
           />
         </div>
         <div className="carousel-container">
-      {salas.length > 0 && !isLoadingSalas && (
-        <>
-          <h1>SALAS</h1>
-          <ImageSlider
-            images={salas.map((sala) => ({
-              id: sala.idSala,
-              url: sala.fotoURL,
-              title: sala.nombre,
-            }))}
-            options={OPTIONS}
-          />
-        </>
-      )}
-    </div>
-    <div className="carousel-container">
-      {experiences.length > 0 && !isLoadingExperiences && (
-        <>
-          <h1>PRÁCTICAS AUTODIRIGIDAS</h1>
-          <ImageSlider
-            images={experiences.map((experience) => ({
-              id: experience.idExperiencia,
-              url: experience.portadaURL,
-              title: experience.nombre,
-            }))}
-            options={OPTIONS}
-          />
-        </>
-      )}
-    </div>
-    <div className="carousel-container">
-      {ufs.length > 0 && !isLoadingUfs && (
-        <>
-          <h1>UNIDADES DE FORMACIÓN</h1>
-          <ImageSlider
-            images={ufs.map((uf) => ({
-              
-              id: uf.idExperiencia,
-              url: uf.portadaURL,
-              title: uf.nombre,
-            }))}
-            options={OPTIONS}
-          />
-        </>
-      )}
-    </div>
+          {salas.length > 0 && !isLoadingSalas && (
+            <>
+              <h1>SALAS</h1>
+              <ImageSlider
+                images={salas.map((sala) => ({
+                  id: sala.idSala,
+                  url: sala.fotoURL,
+                  title: sala.nombre,
+                }))}
+                options={OPTIONS}
+              />
+            </>
+          )}
+        </div>
+        <div className="carousel-container">
+          {experiences.length > 0 && !isLoadingExperiences && (
+            <>
+              <h1>PRÁCTICAS AUTODIRIGIDAS</h1>
+              <ImageSlider
+                images={experiences.map((experience) => ({
+                  id: experience.idExperiencia,
+                  url: experience.portadaURL,
+                  title: experience.nombre,
+                }))}
+                options={OPTIONS}
+              />
+            </>
+          )}
+        </div>
+        <div className="carousel-container">
+          {ufs.length > 0 && !isLoadingUfs && (
+            <>
+              <h1>UNIDADES DE FORMACIÓN</h1>
+              <ImageSlider
+                images={ufs.map((uf) => ({
+                  id: uf.idExperiencia,
+                  url: uf.portadaURL,
+                  title: uf.nombre,
+                }))}
+              />
+            </>
+          )}
+        </div>
       </div>
     </>
   );
