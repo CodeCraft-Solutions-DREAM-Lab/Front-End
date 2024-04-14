c73722088c6a7b20e3dc590a90782e17c8dbe7e1
efrainmartinezgarza <a01280601@tec.mx>
2024-04-14 03:38:52 -0600

    Corrección Logotipo

--- DREAM Lab Frontend/src/Profile/CancelarReservacion.jsx
+++ DREAM Lab Frontend/src/Profile/CancelarReservacion.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import './CancelarReservacion.css';
 import errorLogo from "../Home/Images/errorLogo.png"
+import correctLogo from "../Home/Images/correctLogo.png"
+import infoLogo from "../Home/Images/infoLogo.png"
+
 import BotonNodal from "./BotonNodal"
 
 function CancelarReservacion(props){
+
     // Constantes para los nombres de clase que pueden ser modificados por props
     const blurSpaceClass = `blur-space-tipo2`;
     const nodalBlancoClass = `nodal-blanco-${props.type}`;
-    console.log(nodalBlancoClass)
     const logoNodalOutClass = `logo-nodal-out-tipo2`;
     const logoNodalClass = `logo-nodal-tipo2`;
     const titulo1NodalClass = `titulo1-nodal-${props.type}`;
@@ -15,13 +18,22 @@ function CancelarReservacion(props){
     const titulo3NodalClass = `titulo3-nodal-${props.type}`;
     const titulo4NodalClass = `titulo4-nodal-tipo2`;
     const botonesCancelacionClass = `botones-cancelacion-${props.type}`;
+    let logo = errorLogo;
+
+    if(props.nodalClasificacion == 1){
+        logo = infoLogo
+    } else if (props.nodalClasificacion == 2){
+        logo = errorLogo
+    } else if (props.nodalClasificacion == 3){
+        logo = correctLogo
+    }
 
     return(
         <div className={blurSpaceClass}>
             <div className={nodalBlancoClass}>
                 <div className={logoNodalOutClass}>
-                    {props.nodalClasificacion !== 3 && (
-                        <div className={logoNodalClass}><img src={errorLogo} alt="Icono de notificación" /></div> 
+                    {props.nodalClasificacion !== 4 && (
+                        <div className={logoNodalClass}><img src={logo} alt="Icono de notificación" /></div> 
                     )}
                 </div>
                 <h1 className={titulo1NodalClass}>{props.titulo1}</h1>
