7ff1c2de945333a905e8c29af5029219793081a6
Jinellie <89491839+Jinellie@users.noreply.github.com>
2024-04-16 18:34:57 -0600

    Estructura básica de la pantalla de reservación

--- DREAM Lab Frontend/src/Reservaciones/SelectorSala.jsx
+++ DREAM Lab Frontend/src/Reservaciones/SelectorSala.jsx
@@ -5,69 +5,80 @@ import PrimerRecordatorio from "./components/PrimerRecordatorio";
 import TextoFecha from "./components/TextoFecha";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "../Reservaciones/SelectorSala.css";
+import NavBar from "../components/general/NavBar";
+import Slider from "./Slider";
+import imagePlaceholder from "./components/3D-model-placeholder.png";
 
-import {
-    getFromSessionStorage,
-} from "../Global/Storage";
+import { getFromSessionStorage } from "../Global/Storage";
 
 function SelectorSala(props) {
-    let navigate = useNavigate();
+	let navigate = useNavigate();
 
-    const [isFirstReminderOpen, setIsFirstReminderOpen] = useState(false);
-    const [isNextButtonDisabled, setIsNextButtonDisabled] = useState(false);
-    const [update, setUpdate] = useState(false);
+	const [isFirstReminderOpen, setIsFirstReminderOpen] = useState(false);
+	const [isNextButtonDisabled, setIsNextButtonDisabled] = useState(false);
+	const [update, setUpdate] = useState(false);
 
-    useEffect(() => {
-        if (!getFromSessionStorage("fecha") ||
-            !getFromSessionStorage("horaInicio") || 
-            !getFromSessionStorage("duration")) {
-            setIsNextButtonDisabled(true);
-        } else {
-            setIsNextButtonDisabled(false);
-        }
-    }, [update]);
+	useEffect(() => {
+		if (
+			!getFromSessionStorage("fecha") ||
+			!getFromSessionStorage("horaInicio") ||
+			!getFromSessionStorage("duration")
+		) {
+			setIsNextButtonDisabled(true);
+		} else {
+			setIsNextButtonDisabled(false);
+		}
+	}, [update]);
 
-    return (
-        <div>
-            <Grid container justify="center">
-                <Grid item xs={12} md={7}>
-                    <Card>
-                        <h1>Primera parte</h1>
-                    </Card>
-                </Grid>
-                <Grid item xs={12} md={5}>
-                    <PrimerRecordatorio 
-                        isOpen={isFirstReminderOpen}
-                        size="lg"
-                        onClose={() => {
-                            setIsFirstReminderOpen(false)
-                        }}
-                        onOk={() => {
-                            setIsFirstReminderOpen(false)
-                            navigate("/reservacion/resumen")
-                        }}
-                    />
-                    <FechaFormulario 
-                        update={update}
-                        setUpdate={setUpdate}
-                    />
-                    <TextoFecha update={update}/>
-                    <div className="flex w-fit p-3">
-                    <Button 
-                        className="mt-4 px-2 justify-self-center"
-                        onClick={() => {
-                            setIsFirstReminderOpen(true)
-                        }}
-                        disabled={isNextButtonDisabled}
-                    >
-                        Aceptar
-                    </Button>
-                    </div>
-                </Grid>
-            </Grid>
+	return (
+		<div>
+            <NavBar view="soloPerfil" autoHide={false} />
+			<div className="container">
+				{/* Contenedor principal */}
+				<div className="card-container">
+					{/* Sección de la izquierda */}
 
-        </div>
-    );
+                    {/* Nombre de la sala */}
+					<div>
+						<h1>Nombre de la sala</h1>
+					</div>
+                    <Slider minimo="1" maximo="8"/>
+                    <img src={imagePlaceholder} ></img> {/* Placeholder del modelo 3D */}
+				</div>
+				<div className="form-container">
+					{" "}
+					{/* Sección de la derecha */}
+					<PrimerRecordatorio
+						isOpen={isFirstReminderOpen}
+						size="lg"
+						onClose={() => {
+							setIsFirstReminderOpen(false);
+						}}
+						onOk={() => {
+							setIsFirstReminderOpen(false);
+							navigate("/reservacion/resumen");
+						}}
+					/>
+					<FechaFormulario update={update} setUpdate={setUpdate} />
+					<TextoFecha update={update} />
+					<div className="button-container">
+						{" "}
+						{/* Button container div */}
+						<Button
+							className="mt-4 px-2 justify-self-center"
+							onClick={() => {
+								setIsFirstReminderOpen(true);
+							}}
+							disabled={isNextButtonDisabled}
+						>
+							Aceptar
+						</Button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default SelectorSala;
