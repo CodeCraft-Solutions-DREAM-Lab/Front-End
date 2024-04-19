b1a2c7ffb6a533e1955bc1f079cb35126d8f35f8
Christopher Pedraza <gabalixreborn@gmail.com>
2024-04-19 00:03:34 -0600

    Cambiar fechas de modal de cancelar reservacion para que concuerden en flujo

--- DREAM Lab Frontend/src/Profile/Profile.jsx
+++ DREAM Lab Frontend/src/Profile/Profile.jsx
@@ -11,159 +11,170 @@ import Navbar from "../components/general/NavBar.jsx";
 import { get } from "../Home/Database.js";
 
 const logrosData = [
-  {
-    nombre: "Big Dreamer",
-    descripcion: "Asiste a más de 35 eventos en el Dream Lab.",
-    progreso: 0.75,
-    color: "#7885F8",
-    logo: "LogoBigDreamer",
-  },
-  {
-    nombre: "Robot Expert",
-    descripcion: "Asiste a 3 talleres en el Dimesion Forge.",
-    progreso: 1,
-    color: "#7885F8",
-    logo: "LogoRobot",
-  },
-  {
-    nombre: "Apple Developer",
-    descripcion: "Asiste a 3 talleres de Swift.",
-    progreso: 0.25,
-    color: "#7885F8",
-    logo: "LogoBigDreamer",
-  },
+    {
+        nombre: "Big Dreamer",
+        descripcion: "Asiste a más de 35 eventos en el Dream Lab.",
+        progreso: 0.75,
+        color: "#7885F8",
+        logo: "LogoBigDreamer",
+    },
+    {
+        nombre: "Robot Expert",
+        descripcion: "Asiste a 3 talleres en el Dimesion Forge.",
+        progreso: 1,
+        color: "#7885F8",
+        logo: "LogoRobot",
+    },
+    {
+        nombre: "Apple Developer",
+        descripcion: "Asiste a 3 talleres de Swift.",
+        progreso: 0.25,
+        color: "#7885F8",
+        logo: "LogoBigDreamer",
+    },
 ];
 
 const reservacionesData = [
-  {
-    sala: "Sala Horizons",
-    experiencia: "VR Experience",
-    horaInicio: "15:00:00",
-    duracion: 2,
-    fecha: "2024-12-15",
-  },
-  {
-    sala: "Dimension Forge",
-    experiencia: "Make a Computer - Course",
-    horaInicio: "14:30:00",
-    duracion: 1,
-    fecha: "2024-01-24",
-  },
-  {
-    sala: "Lego Room",
-    experiencia: "Swift Course",
-    horaInicio: "08:00:00",
-    duracion: 3,
-    fecha: "2024-04-26",
-  },
+    {
+        sala: "Sala Horizons",
+        experiencia: "VR Experience",
+        horaInicio: "15:00:00",
+        duracion: 2,
+        fecha: "2024-12-15",
+    },
+    {
+        sala: "Dimension Forge",
+        experiencia: "Make a Computer - Course",
+        horaInicio: "14:30:00",
+        duracion: 1,
+        fecha: "2024-01-24",
+    },
+    {
+        sala: "Lego Room",
+        experiencia: "Swift Course",
+        horaInicio: "08:00:00",
+        duracion: 3,
+        fecha: "2024-04-26",
+    },
 ];
 
 function Profile() {
-  const [reservaciones, setReservaciones] = useState([]);
-  const [tipoNodal, setTipoNodal] = useState(0);
+    const [reservaciones, setReservaciones] = useState([]);
+    const [tipoNodal, setTipoNodal] = useState(0);
 
-  const handleClickNodal = (tipo) => {
-    setTipoNodal(tipo);
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const result = await get("reservaciones");
-        setReservaciones(result);
-        console.log(reservaciones);
-      } catch (error) {
-        console.error("An error occurred:", error);
-      }
+    const handleClickNodal = (tipo) => {
+        setTipoNodal(tipo);
     };
 
-    fetchData();
-  }, []);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const result = await get("reservaciones");
+                setReservaciones(result);
+                console.log(reservaciones);
+            } catch (error) {
+                console.error("An error occurred:", error);
+            }
+        };
 
-  return (
-    <div className="profile-container">
+        fetchData();
+    }, []);
 
-      <div className="navbar-div-profile">
-        <Navbar autoHide={false} view="perfil" />
-      </div>
+    return (
+        <div className="profile-container">
+            <div className="navbar-div-profile">
+                <Navbar autoHide={false} view="perfil" />
+            </div>
 
-      {tipoNodal === 1 ? (
-        <CancelarReservacion
-          nodalClasificacion={1}
-          type="tipo1"
-          titulo1="Tu reserva: Sala Horizons"
-          titulo2="Martes - 15 de diciembre"
-          titulo3="15:00 - 17:00"
-          textoRojo="Cancelar"
-          textoVerde="Ok"
-          funcionRojo={() => handleClickNodal(2)}
-          funcionVerde={() => handleClickNodal(0)}
-        />
-      ) : tipoNodal === 2 ? (
-        <CancelarReservacion
-          nodalClasificacion={2}
-          type="tipo2"
-          titulo1="Estas a punto de cancelar tu reservación en: Dimension Forge"
-          titulo2="Miércoles - 24 de enero"
-          titulo3="14:30 - 15:30"
-          titulo4="¿Deseas continuar?"
-          textoRojo="Si, cancelar"
-          textoVerde="No"
-          funcionRojo={() => handleClickNodal(3)}
-          funcionVerde={() => handleClickNodal(0)}
-        />
-      ) : tipoNodal === 3 ? (
-        <CancelarReservacion
-          nodalClasificacion={3}
-          type="tipo3"
-          titulo1="Cancelación exitosa"
-          titulo2="¡Listo! Tu cancelación se ha procesado con éxito."
-          titulo3="¿Necesitas ayuda? Contáctanos aquí"
-          textoVerde="Cerrar"
-          funcionVerde={() => handleClickNodal(0)}
-        />
-      ) : null}
+            {tipoNodal === 1 ? (
+                <CancelarReservacion
+                    nodalClasificacion={1}
+                    type="tipo1"
+                    titulo1="Tu reserva: Sala Horizons"
+                    titulo2="Viernes - 19 de abril"
+                    titulo3="14:30 - 15:30"
+                    textoRojo="Cancelar"
+                    textoVerde="Ok"
+                    funcionRojo={() => handleClickNodal(2)}
+                    funcionVerde={() => handleClickNodal(0)}
+                />
+            ) : tipoNodal === 2 ? (
+                <CancelarReservacion
+                    nodalClasificacion={2}
+                    type="tipo2"
+                    titulo1="Estas a punto de cancelar tu reservación en: Dimension Forge"
+                    titulo2="Viernes - 19 de abril"
+                    titulo3="14:30 - 15:30"
+                    titulo4="¿Deseas continuar?"
+                    textoRojo="Si, cancelar"
+                    textoVerde="No"
+                    funcionRojo={() => handleClickNodal(3)}
+                    funcionVerde={() => handleClickNodal(0)}
+                />
+            ) : tipoNodal === 3 ? (
+                <CancelarReservacion
+                    nodalClasificacion={3}
+                    type="tipo3"
+                    titulo1="Cancelación exitosa"
+                    titulo2="¡Listo! Tu cancelación se ha procesado con éxito."
+                    titulo3="¿Necesitas ayuda? Contáctanos aquí"
+                    textoVerde="Cerrar"
+                    funcionVerde={() => handleClickNodal(0)}
+                />
+            ) : null}
 
-      {/* <div className="boton-atras">
+            {/* <div className="boton-atras">
     				<BotonBack className="imagen-boton" ruta="/home/"/>
 			</div> */}
 
-      <NameCard />
+            <NameCard />
 
-      <div className="div-exterior-perfil">
-        <div className="esfera-div-celular">
-          <EsferaPuntosPrioridad puntos="367" subtitulo="Puntos de prioridad" />
+            <div className="div-exterior-perfil">
+                <div className="esfera-div-celular">
+                    <EsferaPuntosPrioridad
+                        puntos="367"
+                        subtitulo="Puntos de prioridad"
+                    />
+                </div>
+
+                <div className="logros-div">
+                    <h2 className="sub">Logros</h2>
+
+                    <div className="logros-div-in">
+                        {renderTarjetasLogro(logrosData)}
+                        <div className="degradado-down"></div>
+                    </div>
+                </div>
+
+                <div className="esfera-div">
+                    <EsferaPuntosPrioridad
+                        puntos="367"
+                        subtitulo="Puntos de prioridad"
+                    />
+                </div>
+
+                <div className="reservaciones-div">
+                    <h2 className="sub">Reservaciones activas</h2>
+
+                    <div className="reservaciones-div-in">
+                        {generateReservationCards(
+                            reservaciones,
+                            handleClickNodal
+                        )}
+                        <div className="degradado-down"></div>
+                    </div>
+                </div>
+
+                <div className="botones-modo-celular">
+                    <BotonCelular texto="Logros" tipo="logros" />
+                    <BotonCelular
+                        texto="Reservaciones activas"
+                        tipo="calendario"
+                    />
+                </div>
+            </div>
         </div>
-
-        <div className="logros-div">
-          <h2 className="sub">Logros</h2>
-
-          <div className="logros-div-in">
-            {renderTarjetasLogro(logrosData)}
-            <div className="degradado-down"></div>
-          </div>
-        </div>
-
-        <div className="esfera-div">
-          <EsferaPuntosPrioridad puntos="367" subtitulo="Puntos de prioridad" />
-        </div>
-
-        <div className="reservaciones-div">
-          <h2 className="sub">Reservaciones activas</h2>
-
-          <div className="reservaciones-div-in">
-            {generateReservationCards(reservaciones, handleClickNodal)}
-            <div className="degradado-down"></div>
-          </div>
-        </div>
-
-        <div className="botones-modo-celular">
-          <BotonCelular texto="Logros" tipo="logros" />
-          <BotonCelular texto="Reservaciones activas" tipo="calendario" />
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Profile;
