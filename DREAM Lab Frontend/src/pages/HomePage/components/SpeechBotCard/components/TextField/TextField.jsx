30b5d1f661090da30cfc009b4d0f29ddc2854495
Christopher Pedraza <gabalixreborn@gmail.com>
2024-04-20 11:05:05 -0600

    Refactor TextField

--- DREAM Lab Frontend/src/pages/HomePage/components/SpeechBotCard/components/TextField/TextField.jsx
+++ DREAM Lab Frontend/src/pages/HomePage/components/SpeechBotCard/components/TextField/TextField.jsx
@@ -1,26 +1,28 @@
 import React, { useRef, useEffect } from "react";
 
+import "./TextField.css";
+
 function TextField({ value, onChange, onKeyDown }) {
-  const inputRef = useRef(null);
+    const inputRef = useRef(null);
 
-  useEffect(() => {
-    // Scroll the input field to the end whenever the value changes
-    if (inputRef.current) {
-      inputRef.current.scrollLeft = inputRef.current.scrollWidth;
-    }
-  }, [value]);
+    useEffect(() => {
+        // Scroll the input field to the end whenever the value changes
+        if (inputRef.current) {
+            inputRef.current.scrollLeft = inputRef.current.scrollWidth;
+        }
+    }, [value]);
 
-  return (
-    <input
-      ref={inputRef}
-      name="asistente de voz"
-      className="text-field"
-      value={value}
-      onChange={onChange}
-      onKeyDown={onKeyDown}
-      placeholder="Escribe o habla para pedir una recomendación"
-    />
-  );
+    return (
+        <input
+            ref={inputRef}
+            name="asistente de voz"
+            className="text-field"
+            value={value}
+            onChange={onChange}
+            onKeyDown={onKeyDown}
+            placeholder="Escribe o habla para pedir una recomendación"
+        />
+    );
 }
 
 export default TextField;
