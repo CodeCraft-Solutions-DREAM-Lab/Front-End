27ea5f3a92cd6b8754d3c34cbd5eeb37de9d8d97
Christopher Pedraza <gabalixreborn@gmail.com>
2024-04-20 21:09:17 -0600

    Mover imagenes de public  sus componenntes

--- DREAM Lab Frontend/src/pages/HomePage/components/RecomendacionesInvalidas/RecomendacionesInvalidas.jsx
+++ DREAM Lab Frontend/src/pages/HomePage/components/RecomendacionesInvalidas/RecomendacionesInvalidas.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React from "react";
 import "./RecomendacionesInvalidas.css";
 
 function RecomendacionesInvalidas() {
-  return (
-    <div className="avisoRecomendacionesInvalidas">
-      <div className="containerFondo">
-        <img src="/nube_error.svg" alt="Nube triste" className="nubeError"/>
-        <p>Lo sentimos, no pudimos encontrar ningún resultado. Intenta otra búsqueda.</p>
-      </div>
-    </div>
-  );
+    return (
+        <div className="avisoRecomendacionesInvalidas">
+            <div className="containerFondo">
+                <img
+                    src="/src/pages/Homepage/assets/images/nube_error.svg"
+                    alt="Nube triste"
+                    className="nubeError"
+                />
+                <p>
+                    Lo sentimos, no pudimos encontrar ningún resultado. Intenta
+                    otra búsqueda.
+                </p>
+            </div>
+        </div>
+    );
 }
 
 export default RecomendacionesInvalidas;
-
