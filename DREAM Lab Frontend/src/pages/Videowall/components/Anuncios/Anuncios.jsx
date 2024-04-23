67bb88486e23d0be3f6046da79e6736429f83380
GReyes4 <105297867+GReyes4@users.noreply.github.com>
2024-04-23 08:02:43 -0600

    Add Info por props

--- DREAM Lab Frontend/src/pages/Videowall/components/Anuncios/Anuncios.jsx
+++ DREAM Lab Frontend/src/pages/Videowall/components/Anuncios/Anuncios.jsx
@@ -7,23 +7,23 @@ const anunciosDefault = [
     {
         id: 1,
         img: "https://media.licdn.com/dms/image/C4E12AQHTZ7pyTmCfEw/article-cover_image-shrink_720_1280/0/1541423067327?e=2147483647&v=beta&t=4fBTi39dOwxthLmtYW4RGwseAA9kAXBPUdnQa_7PKx4",
-        nombre: "Lab Default 1",
-        sala: "Sala Default 1",
+        title: "Taller de impresión 3D",
+        sala: "Dimension Forge",
         date: "Lunes 15 de Febrero 3:00pm a 5:00pm",
     },
     {
         id: 2,
         img: "https://mms.businesswire.com/media/20190617005571/es/727944/5/Warner_Final_Zone5.1.jpg",
-        nombre: "Lab Default 2",
-        sala: "Sala Default 2",
-        date: "Lunes 15 de Febrero 3:00pm a 5:00pm",
+        title: "Curso de Swift",
+        sala: "New Horizons",
+        date: "Martes 16 de Febrero 3:00pm a 5:00pm",
     },
     {
         id: 3,
         img: "https://d1tm14lrsghf7q.cloudfront.net/public/media/23459/conversions/21386-thumb.jpg",
-        nombre: "Lab Default 3",
-        sala: "Sala Default 3",
-        date: "Lunes 15 de Febrero 3:00pm a 5:00pm",
+        title: "Seguridad en la Red",
+        sala: "Electric Garage",
+        date: "Miércoles 17 de Febrero 3:00pm a 5:00pm",
     },
 ];
 
@@ -87,7 +87,7 @@ function Anuncios() {
                             }}
                         >
                             <Hora/>
-                            <InfoEvento/>
+                            <InfoEvento {...item}/>
                         </div>
                     </React.Fragment>
                 ))}
--- DREAM Lab Frontend/src/pages/Videowall/components/Anuncios/components/InfoEvento/InfoEvento.jsx
+++ DREAM Lab Frontend/src/pages/Videowall/components/Anuncios/components/InfoEvento/InfoEvento.jsx
@@ -1,13 +1,13 @@
 import "./InfoEvento.css";
 import GlassCard from "src/GlobalComponents/GlassCard/GlassCard";
 
-function InfoEvento() {
+function InfoEvento(props) {
     return (
         <div className="info-container">
             <GlassCard borderRadius="2vh" classes={"info-glasscard"}>
-                <h1 className="info-title">Taller de impresión 3D</h1>
-                <p className="info-sala">Dimension Forge</p>
-                <p className="info-date">Lunes 15 de Febrero 3:00pm a 5:00pm</p>
+                <h1 className="info-title">{props.title}</h1>
+                <p className="info-sala">{props.sala}</p>
+                <p className="info-date">{props.date}</p>
             </GlassCard>
         </div>
     );
