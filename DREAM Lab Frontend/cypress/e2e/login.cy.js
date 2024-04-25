b4affa0e379d91c6fca390db0192d9e29ca6b811
Christopher Pedraza <gabalixreborn@gmail.com>
2024-04-25 13:43:22 -0600

    Configurar cypress para pruebas e2e

---
+++ DREAM Lab Frontend/cypress/e2e/login.cy.js
@@ -0,0 +1,7 @@
+describe("Pruebas de login", () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:5173/login");
+    });
+
+    it("Ingresar con credenciales válidas", () => {});
+});
