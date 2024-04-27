87d78b530ea948f3830a0150bdbcfc09150bb365
Christopher Pedraza <gabalixreborn@gmail.com>
2024-04-27 11:11:05 -0600

    Agregar comandos y modificar pruebas de login para usarlos

--- dream-lab-frontend/cypress/e2e/login.cy.js
+++ dream-lab-frontend/cypress/e2e/login.cy.js
@@ -4,48 +4,44 @@ describe("Pruebas de login", () => {
     });
 
     it("Dejar todos los campos en blanco", () => {
-        cy.get("[data-cy=login-button]").click();
-        cy.get("[data-cy=login-error]").contains("Introduce un usuario");
+        cy.clickDataCy("login-button");
+        cy.getDataCy("login-error").contains("Introduce un usuario");
     });
 
     it("Dejar el campo de usuario en blanco", () => {
-        cy.get("[data-cy=login-password]").type("test");
-        cy.get("[data-cy=login-button]").click();
-        cy.get("[data-cy=login-error]").contains("Introduce un usuario");
+        cy.typeDataCy("login-password", "test");
+        cy.clickDataCy("login-button");
+        cy.getDataCy("login-error").contains("Introduce un usuario");
     });
 
     it("Dejar el campo de contraseña en blanco", () => {
-        cy.get("[data-cy=login-user]").type("test");
-        cy.get("[data-cy=login-button]").click();
-        cy.get("[data-cy=login-error]").contains("Introduce una contraseña");
+        cy.typeDataCy("login-user", "test");
+        cy.clickDataCy("login-button");
+        cy.getDataCy("login-error").contains("Introduce una contraseña");
     });
 
     it("Modificar visibilidad de contraseña", () => {
-        cy.get("[data-cy=login-user]").type("test");
-        cy.get("[data-cy=login-password]").type("test");
-        cy.get("[data-cy=login-password-visibility]").click();
-        cy.get("[data-cy=login-password]").should("have.attr", "type", "text");
-        cy.get("[data-cy=login-password-visibility]").click();
-        cy.get("[data-cy=login-password]").should(
-            "have.attr",
-            "type",
-            "password"
-        );
+        cy.typeDataCy("login-user", "test");
+        cy.typeDataCy("login-password", "test");
+        cy.clickDataCy("login-password-visibility");
+        cy.getDataCy("login-password").hasAttribute("type", "text");
+        cy.clickDataCy("login-password-visibility");
+        cy.getDataCy("login-password").hasAttribute("type", "password");
     });
 
     it("Usuario y contraseña incorrectos", () => {
-        cy.get("[data-cy=login-user]").type("usuario");
-        cy.get("[data-cy=login-password]").type("contraseña");
-        cy.get("[data-cy=login-button]").click();
-        cy.get("[data-cy=login-error]").contains(
+        cy.typeDataCy("login-user", "usuario");
+        cy.typeDataCy("login-password", "contraseña");
+        cy.clickDataCy("login-button");
+        cy.getDataCy("login-error").contains(
             "Usuario o contraseña incorrectos"
         );
     });
 
     it("Usuario y contraseña correctos", () => {
-        cy.get("[data-cy=login-user]").type("test");
-        cy.get("[data-cy=login-password]").type("test");
-        cy.get("[data-cy=login-button]").click();
+        cy.typeDataCy("login-user", "test");
+        cy.typeDataCy("login-password", "test");
+        cy.clickDataCy("login-button");
         cy.get(".navbar").contains("DREAM LAB");
     });
 });
