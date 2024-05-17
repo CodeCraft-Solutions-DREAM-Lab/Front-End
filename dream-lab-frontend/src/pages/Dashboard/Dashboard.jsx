3c88574bf57ff3e4f6f6f9f502e9ec8ff91f96c9
Christopher Pedraza <gabalixreborn@gmail.com>
2024-05-16 20:16:03 -0600

    Modificar clases de tarjeta de stats

--- dream-lab-frontend/src/pages/Dashboard/Dashboard.jsx
+++ dream-lab-frontend/src/pages/Dashboard/Dashboard.jsx
@@ -8,10 +8,18 @@ function Dashboard() {
     return (
         <div className="p-8">
             <div className="dashboard-statcards-container">
-                <StatCard nombre="Usuarios" valor={100} cambio={10} />
-                <StatCard nombre="Reservaciones" valor={100} cambio={10} />
-                <StatCard nombre="Salas" valor={100} cambio={-10} />
-                <StatCard nombre="Materiales" valor={100} cambio={-10} />
+                <StatCard
+                    nombre="Reservaciones totales"
+                    valor={100}
+                    cambio={10}
+                />
+                <StatCard
+                    nombre="Reservaciones activas"
+                    valor={100}
+                    cambio={10}
+                />
+                <StatCard nombre="Penalizaciones" valor={100} cambio={0} />
+                <StatCard nombre="Cancelaciones" valor={100} cambio={-10} />
             </div>
         </div>
     );
--- dream-lab-frontend/src/pages/Dashboard/components/StatCard/StatCard.jsx
+++ dream-lab-frontend/src/pages/Dashboard/components/StatCard/StatCard.jsx
@@ -3,30 +3,35 @@ import { propTypes } from "react-bootstrap/esm/Image";
 // Icons
 import stat_arrow_down from "src/assets/Admin/stat_arrow_down.svg";
 import stat_arrow_up from "src/assets/Admin/stat_arrow_up.svg";
+import stat_no_change from "src/assets/Admin/stat_no_change.svg";
 
 // Estilos
 import "./StatCard.css";
 
 function StatCard({ nombre, valor, cambio }) {
+    cambio = cambio < 0 ? cambio : `+${cambio}`;
+
+    let imagen = stat_no_change;
+    if (cambio < 0) {
+        imagen = stat_arrow_down;
+    } else if (cambio > 0) {
+        imagen = stat_arrow_up;
+    }
+
     return (
         <div className="sc-card-container">
-            <div className="sc-card-header">
-                <h1>{nombre}</h1>
+            <div>
+                <h1 className="sc-card-header">{nombre}</h1>
             </div>
-            <div className="flex flex-row justify-between">
-                <span>{valor}</span>
+            <div className="sc-card-value-container">
+                <span className="sc-card-value">{valor}</span>
 
-                {cambio < 0 ? (
-                    <div className="flex flex-row">
+                <div className="sc-card-value-change-container">
+                    <div>
                         <span>{cambio}%</span>
-                        <img src={stat_arrow_down} alt="arrow down" />
                     </div>
-                ) : (
-                    <div className="flex flex-row">
-                        <span>+{cambio}%</span>
-                        <img src={stat_arrow_up} alt="arrow up" />
-                    </div>
-                )}
+                    <img src={imagen} alt="arrow" />
+                </div>
             </div>
         </div>
     );
