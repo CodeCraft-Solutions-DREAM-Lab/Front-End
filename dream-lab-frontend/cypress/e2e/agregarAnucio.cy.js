43f522acfc2aa5b9f59fd5ac415c001a73dc5e07
Christopher Pedraza <gabalixreborn@gmail.com>
2024-05-25 10:56:54 -0600

    Probar remover algo de los intecepts

--- dream-lab-frontend/cypress/e2e/agregarAnucio.cy.js
+++ dream-lab-frontend/cypress/e2e/agregarAnucio.cy.js
@@ -75,47 +75,41 @@ describe("Creación de anuncios para el video wall", () => {
         cy.wait("@postAnuncioEvento");
 
         // Simular una respuesta mock para el segundo GET con los datos actualizados
-        cy.intercept(
-            "GET",
-            "https://readanuncios-j5zt2ysdwq-uc.a.run.app/",
-            (req) => {
-                req.reply({
-                    statusCode: 200,
-                    body: [
-                        {
-                            descripcion: "",
-                            encendido: true,
-                            fecha: "12 de junio, 2024",
-                            horaFin: "17:00",
-                            horaInicio: "15:30",
-                            nombreEvento: "Taller: Un vistazo al pasado",
-                            nombreSala: "Graveyard",
-                            personalizado: false,
-                            posicion: 0,
-                            soloImagen: false,
-                            urlImagen:
-                                "https://firebasestorage.googleapis.com/v0/b/dream-lab-videowall.appspot.com/o/c4b37173-a86b-4d38-9ee4-ef55f884c74f?alt=media&token=00f9b6a5-c6b3-4673-8435-4b234e43eff0",
-                            firebaseId: "9wrY3foxJAda6ucXuan",
-                        },
-                        {
-                            descripcion: "",
-                            encendido: true,
-                            fecha: "5 de mayo, 2024",
-                            horaInicio: "09:00",
-                            horaFin: "10:00",
-                            nombreEvento: "Anuncio de prueba",
-                            nombreSala: "Ubicación de prueba",
-                            personalizado: false,
-                            posicion: 1,
-                            soloImagen: false,
-                            urlImagen:
-                                "https://firebasestorage.googleapis.com/v0/b/dream-lab-videowall.appspot.com/o/a6e3f903-26c6-4b0b-9fcf-15b2f68a1430?alt=media&token=d2e3c48c-f3b1-4602-b262-b9a4439a8272",
-                            firebaseId: "9wgjdfsoxJAda6u493an",
-                        },
-                    ],
-                });
-            }
-        ).as("getAnunciosActualizado");
+        cy.intercept("GET", "https://readanuncios-j5zt2ysdwq-uc.a.run.app/", {
+            statusCode: 200,
+            body: [
+                {
+                    descripcion: "",
+                    encendido: true,
+                    fecha: "12 de junio, 2024",
+                    horaFin: "17:00",
+                    horaInicio: "15:30",
+                    nombreEvento: "Taller: Un vistazo al pasado",
+                    nombreSala: "Graveyard",
+                    personalizado: false,
+                    posicion: 0,
+                    soloImagen: false,
+                    urlImagen:
+                        "https://firebasestorage.googleapis.com/v0/b/dream-lab-videowall.appspot.com/o/c4b37173-a86b-4d38-9ee4-ef55f884c74f?alt=media&token=00f9b6a5-c6b3-4673-8435-4b234e43eff0",
+                    firebaseId: "9wrY3foxJAda6ucXuan",
+                },
+                {
+                    descripcion: "",
+                    encendido: true,
+                    fecha: "5 de mayo, 2024",
+                    horaInicio: "09:00",
+                    horaFin: "10:00",
+                    nombreEvento: "Anuncio de prueba",
+                    nombreSala: "Ubicación de prueba",
+                    personalizado: false,
+                    posicion: 1,
+                    soloImagen: false,
+                    urlImagen:
+                        "https://firebasestorage.googleapis.com/v0/b/dream-lab-videowall.appspot.com/o/a6e3f903-26c6-4b0b-9fcf-15b2f68a1430?alt=media&token=d2e3c48c-f3b1-4602-b262-b9a4439a8272",
+                    firebaseId: "9wgjdfsoxJAda6u493an",
+                },
+            ],
+        }).as("getAnunciosActualizado");
 
         // Esperar a que se complete la solicitud GET
         cy.wait("@getAnunciosActualizado");
@@ -170,47 +164,41 @@ describe("Creación de anuncios para el video wall", () => {
         cy.wait("@postAnuncioPersonalizado");
 
         // Simular una respuesta mock para el segundo GET con los datos actualizados
-        cy.intercept(
-            "GET",
-            "https://readanuncios-j5zt2ysdwq-uc.a.run.app/",
-            (req) => {
-                req.reply({
-                    statusCode: 200,
-                    body: [
-                        {
-                            descripcion: "",
-                            encendido: true,
-                            fecha: "12 de junio, 2024",
-                            horaFin: "17:00",
-                            horaInicio: "15:30",
-                            nombreEvento: "Taller: Un vistazo al pasado",
-                            nombreSala: "Graveyard",
-                            personalizado: false,
-                            posicion: 0,
-                            soloImagen: false,
-                            urlImagen:
-                                "https://firebasestorage.googleapis.com/v0/b/dream-lab-videowall.appspot.com/o/c4b37173-a86b-4d38-9ee4-ef55f884c74f?alt=media&token=00f9b6a5-c6b3-4673-8435-4b234e43eff0",
-                            firebaseId: "9wrYH3foJAda6ucXuan",
-                        },
-                        {
-                            descripcion: "Descripción",
-                            encendido: true,
-                            fecha: "",
-                            horaInicio: "",
-                            horaFin: "",
-                            nombreEvento: "",
-                            nombreSala: "Anuncio de prueba",
-                            personalizado: true,
-                            posicion: 1,
-                            soloImagen: false,
-                            urlImagen:
-                                "https://firebasestorage.googleapis.com/v0/b/dream-lab-videowall.appspot.com/o/a6e3f903-26c6-4b0b-9fcf-15b2f68a1430?alt=media&token=d2e3c48c-f3b1-4602-b262-b9a4439a8272",
-                            firebaseId: "9wgjdfsoxJAda11493an",
-                        },
-                    ],
-                });
-            }
-        ).as("getAnunciosActualizado");
+        cy.intercept("GET", "https://readanuncios-j5zt2ysdwq-uc.a.run.app/", {
+            statusCode: 200,
+            body: [
+                {
+                    descripcion: "",
+                    encendido: true,
+                    fecha: "12 de junio, 2024",
+                    horaFin: "17:00",
+                    horaInicio: "15:30",
+                    nombreEvento: "Taller: Un vistazo al pasado",
+                    nombreSala: "Graveyard",
+                    personalizado: false,
+                    posicion: 0,
+                    soloImagen: false,
+                    urlImagen:
+                        "https://firebasestorage.googleapis.com/v0/b/dream-lab-videowall.appspot.com/o/c4b37173-a86b-4d38-9ee4-ef55f884c74f?alt=media&token=00f9b6a5-c6b3-4673-8435-4b234e43eff0",
+                    firebaseId: "9wrYH3foJAda6ucXuan",
+                },
+                {
+                    descripcion: "Descripción",
+                    encendido: true,
+                    fecha: "",
+                    horaInicio: "",
+                    horaFin: "",
+                    nombreEvento: "",
+                    nombreSala: "Anuncio de prueba",
+                    personalizado: true,
+                    posicion: 1,
+                    soloImagen: false,
+                    urlImagen:
+                        "https://firebasestorage.googleapis.com/v0/b/dream-lab-videowall.appspot.com/o/a6e3f903-26c6-4b0b-9fcf-15b2f68a1430?alt=media&token=d2e3c48c-f3b1-4602-b262-b9a4439a8272",
+                    firebaseId: "9wgjdfsoxJAda11493an",
+                },
+            ],
+        }).as("getAnunciosActualizado");
 
         // Esperar a que se complete la solicitud GET
         cy.wait("@getAnunciosActualizado");
@@ -260,47 +248,41 @@ describe("Creación de anuncios para el video wall", () => {
         cy.wait("@postAnuncioSoloImagen");
 
         // Simular una respuesta mock para el segundo GET con los datos actualizados
-        cy.intercept(
-            "GET",
-            "https://readanuncios-j5zt2ysdwq-uc.a.run.app/",
-            (req) => {
-                req.reply({
-                    statusCode: 200,
-                    body: [
-                        {
-                            descripcion: "",
-                            encendido: true,
-                            fecha: "12 de junio, 2024",
-                            horaFin: "17:00",
-                            horaInicio: "15:30",
-                            nombreEvento: "Taller: Un vistazo al pasado",
-                            nombreSala: "Graveyard",
-                            personalizado: false,
-                            posicion: 0,
-                            soloImagen: false,
-                            urlImagen:
-                                "https://firebasestorage.googleapis.com/v0/b/dream-lab-videowall.appspot.com/o/c4b37173-a86b-4d38-9ee4-ef55f884c74f?alt=media&token=00f9b6a5-c6b3-4673-8435-4b234e43eff0",
-                            firebaseId: "9wrYH3foJAda6ucXuan",
-                        },
-                        {
-                            descripcion: "",
-                            encendido: true,
-                            fecha: "",
-                            horaInicio: "",
-                            horaFin: "",
-                            nombreEvento: "",
-                            nombreSala: "",
-                            personalizado: false,
-                            posicion: 1,
-                            soloImagen: true,
-                            urlImagen:
-                                "https://firebasestorage.googleapis.com/v0/b/dream-lab-videowall.appspot.com/o/a6e3f903-26c6-4b0b-9fcf-15b2f68a1430?alt=media&token=d2e3c48c-f3b1-4602-b262-b9a4439a8272",
-                            firebaseId: "9wgjdfsoxdAda11493an",
-                        },
-                    ],
-                });
-            }
-        ).as("getAnunciosActualizado");
+        cy.intercept("GET", "https://readanuncios-j5zt2ysdwq-uc.a.run.app/", {
+            statusCode: 200,
+            body: [
+                {
+                    descripcion: "",
+                    encendido: true,
+                    fecha: "12 de junio, 2024",
+                    horaFin: "17:00",
+                    horaInicio: "15:30",
+                    nombreEvento: "Taller: Un vistazo al pasado",
+                    nombreSala: "Graveyard",
+                    personalizado: false,
+                    posicion: 0,
+                    soloImagen: false,
+                    urlImagen:
+                        "https://firebasestorage.googleapis.com/v0/b/dream-lab-videowall.appspot.com/o/c4b37173-a86b-4d38-9ee4-ef55f884c74f?alt=media&token=00f9b6a5-c6b3-4673-8435-4b234e43eff0",
+                    firebaseId: "9wrYH3foJAda6ucXuan",
+                },
+                {
+                    descripcion: "",
+                    encendido: true,
+                    fecha: "",
+                    horaInicio: "",
+                    horaFin: "",
+                    nombreEvento: "",
+                    nombreSala: "",
+                    personalizado: false,
+                    posicion: 1,
+                    soloImagen: true,
+                    urlImagen:
+                        "https://firebasestorage.googleapis.com/v0/b/dream-lab-videowall.appspot.com/o/a6e3f903-26c6-4b0b-9fcf-15b2f68a1430?alt=media&token=d2e3c48c-f3b1-4602-b262-b9a4439a8272",
+                    firebaseId: "9wgjdfsoxdAda11493an",
+                },
+            ],
+        }).as("getAnunciosActualizado");
 
         // Esperar a que se complete la solicitud GET
         cy.wait("@getAnunciosActualizado");
