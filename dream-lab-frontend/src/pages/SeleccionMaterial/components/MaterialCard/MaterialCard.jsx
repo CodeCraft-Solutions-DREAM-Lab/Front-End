d53fa144cba81f58f0dfe69b75d8231aed23b38d
ReyHdz <a00829814@itesm.mx>
2024-06-04 01:49:34 -0600

    Mostrar materiales recomendados
    Se muestra un borde azul para materiales recomendados junto con la palabra "RECOMENDADO" arriba del material recomendado. Se agregó el componente de un modal de warning por si no se han seleccionado los materiales recomendados, pero falta implementar la funcionalidad del componente.

--- dream-lab-frontend/src/pages/SeleccionMaterial/components/MaterialCard/MaterialCard.jsx
+++ dream-lab-frontend/src/pages/SeleccionMaterial/components/MaterialCard/MaterialCard.jsx
@@ -1,10 +1,10 @@
 import "./MaterialCard.css";
 import { useState } from "react";
 import PropTypes from "prop-types";
-import plusIcon from "../../../../assets/SeleccionMaterial/plus-icon.svg"
-import minusIcon from "../../../../assets/SeleccionMaterial/minus-icon.svg"
+import plusIcon from "../../../../assets/SeleccionMaterial/plus-icon.svg";
+import minusIcon from "../../../../assets/SeleccionMaterial/minus-icon.svg";
 
-function MaterialCard({ materialId, name, image, hideQuantity, onQuantityUpdate, initialQuantity, maxQuantity }) {
+function MaterialCard({ materialId, name, image, hideQuantity, onQuantityUpdate, initialQuantity, maxQuantity, isRecommended }) {
   const [quantity, setQuantity] = useState(initialQuantity);
 
   const handlePlus = () => {
@@ -24,51 +24,55 @@ function MaterialCard({ materialId, name, image, hideQuantity, onQuantityUpdate,
     }
   };
 
-	return (
-		<>
-			<div className="card">
-				<div className="nombre-material" data-cy="material-name">
-					<h1>{name}</h1>
-				</div>
-				<div className="material-image">
-					<img src={image} alt="Imagen del material" />
-				</div>
-				<div className="icons">
-					{hideQuantity ? (
-						<p>{quantity}</p>
-					) : (
-						<>
-							<img
-								onClick={handleMinus}
-								src={minusIcon}
-								alt="Menos"
-								data-cy="minus-button"
-							/>
-							<div className="quantity" data-cy="quantity">
-								<p>{quantity}</p>
-							</div>
-							<img
-								onClick={handlePlus}
-								src={plusIcon}
-								alt="Más"
-								data-cy="plus-button"
-							/>
-						</>
-					)}
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <div className={`card ${isRecommended ? "recommended-material" : ""}`}>
+      {isRecommended && (
+        <div className="recommended-label">
+          RECOMENDADO
+        </div>
+      )}
+      <div className="nombre-material" data-cy="material-name">
+        <h1>{name}</h1>
+      </div>
+      <div className="material-image">
+        <img src={image} alt="Imagen del material" />
+      </div>
+      <div className="icons">
+        {hideQuantity ? (
+          <p>{quantity}</p>
+        ) : (
+          <>
+            <img
+              onClick={handleMinus}
+              src={minusIcon}
+              alt="Menos"
+              data-cy="minus-button"
+            />
+            <div className="quantity" data-cy="quantity">
+              <p>{quantity}</p>
+            </div>
+            <img
+              onClick={handlePlus}
+              src={plusIcon}
+              alt="Más"
+              data-cy="plus-button"
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
 MaterialCard.propTypes = {
-	materialId: PropTypes.number.isRequired,
-	name: PropTypes.string.isRequired,
-	image: PropTypes.string.isRequired,
-	hideQuantity: PropTypes.bool,
-	onQuantityUpdate: PropTypes.func.isRequired,
-	initialQuantity: PropTypes.number,
-	maxQuantity: PropTypes.number.isRequired
+  materialId: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  hideQuantity: PropTypes.bool,
+  onQuantityUpdate: PropTypes.func.isRequired,
+  initialQuantity: PropTypes.number,
+  maxQuantity: PropTypes.number.isRequired,
+  isRecommended: PropTypes.bool.isRequired
 };
 
 export default MaterialCard;
