027505cebed6fb04d7ade75fdb2cd31e1f425782
Christopher Pedraza <gabalixreborn@gmail.com>
2024-06-04 23:34:38 -0600

    Enrutar a reservacion de sala seleccionada y agregar alertas

--- dream-lab-frontend/src/pages/CronogramaAdmin/components/ModalCrearReservacionAdmin/ModalCrearReservacionAdmin.jsx
+++ dream-lab-frontend/src/pages/CronogramaAdmin/components/ModalCrearReservacionAdmin/ModalCrearReservacionAdmin.jsx
@@ -14,77 +14,143 @@ import {
 // Estilos
 import "./ModalCrearReservacionAdmin.css";
 
+// Router
+import { useNavigate } from "react-router-dom";
+
+// import Storage
+import { saveToSessionStorage } from "src/utils/Storage.js";
+
+// Hooks
+import { useState } from "react";
+
+// Alerts
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
+
 import propTypes from "prop-types";
 
 function ModalCrearReservacionAdmin({ isOpen, onOpen, onOpenChange, salas }) {
-    return (
-        <Modal
-            isOpen={isOpen}
-            onOpenChange={onOpenChange}
-            backdrop="opaque"
-            size="2xl"
-        >
-            <ModalContent>
-                {(onClose) => (
-                    <>
-                        <ModalHeader className="mcra-header">
-                            <h1 className="mcra-title">Crear Reservación</h1>
-                        </ModalHeader>
-                        <ModalBody>
-                            <Input
-                                label="Persona asignada a la reservación"
-                                placeholder="Nombre"
-                                labelPlacement="outside"
-                                className="mcra-input"
-                                classNames={{
-                                    inputWrapper: [
-                                        "bg-transparent border-2 h-12",
-                                    ],
-                                    label: ["text-lg"],
-                                }}
-                            />
-                            <Select
-                                label="Sala"
-                                labelPlacement="outside"
-                                placeholder="Seleccione una sala"
-                                className="mcra-select"
-                                classNames={{
-                                    trigger: ["bg-transparent border-2  h-12"],
-                                    label: ["text-lg"],
-                                }}
-                            >
-                                {salas.map((sala) => (
-                                    <SelectItem key={sala.id} value={sala.id}>
-                                        {sala.nombre}
-                                    </SelectItem>
-                                ))}
-                            </Select>
+    const navigate = useNavigate();
+    const [selectedSala, setSelectedSala] = useState([]);
+    const [nombre, setNombre] = useState("");
+    const [open, setOpen] = useState(false);
+    const [message, setMessage] = useState("");
 
-                            <div className="mcra-contenedor-botones">
-                                <Button
-                                    className="bg-white border-4 border-[#ac3e1b] text-[#ac3e1b] hover:bg-[#ac3e1b] hover:border-[#ac3e1b] hover:text-white mcra-button"
-                                    onClick={onClose}
-                                    radius="full"
+    const handleAceptar = () => {
+        if (nombre === "") {
+            setOpen(true);
+            setMessage("Introduce un nombre");
+            return;
+        }
+        if (selectedSala.length === 0) {
+            setOpen(true);
+            setMessage("Selecciona una sala");
+            return;
+        }
+        onOpenChange();
+
+        const salaSeleccionada = selectedSala.values().next().value;
+        saveToSessionStorage("reservType", "sala");
+        saveToSessionStorage("idSala", salaSeleccionada);
+        navigate(`/reservacion/sala`);
+    };
+
+    const handleClose = (event, reason) => {
+        if (reason === "clickaway") {
+            return;
+        }
+        setOpen(false);
+    };
+
+    return (
+        <>
+            <Modal
+                isOpen={isOpen}
+                onOpenChange={onOpenChange}
+                backdrop="opaque"
+                size="2xl"
+            >
+                <ModalContent>
+                    {(onClose) => (
+                        <>
+                            <ModalHeader className="mcra-header">
+                                <h1 className="mcra-title">
+                                    Crear Reservación
+                                </h1>
+                            </ModalHeader>
+                            <ModalBody>
+                                <Input
+                                    label="Persona asignada a la reservación"
+                                    placeholder="Nombre"
+                                    labelPlacement="outside"
+                                    className="mcra-input"
+                                    classNames={{
+                                        inputWrapper: [
+                                            "bg-transparent border-2 h-12",
+                                        ],
+                                        label: ["text-lg"],
+                                    }}
+                                    value={nombre}
+                                    onValueChange={setNombre}
+                                />
+                                <Select
+                                    label="Sala"
+                                    labelPlacement="outside"
+                                    placeholder="Seleccione una sala"
+                                    className="mcra-select"
+                                    classNames={{
+                                        trigger: [
+                                            "bg-transparent border-2  h-12",
+                                        ],
+                                        label: ["text-lg"],
+                                    }}
+                                    selectedKeys={selectedSala}
+                                    onSelectionChange={setSelectedSala}
                                 >
-                                    <span className="mcra-button-text">
-                                        Cancelar
-                                    </span>
-                                </Button>
-                                <Button
-                                    className="bg-white border-4 border-[#1bac55] text-[#1bac55] hover:bg-[#1bac55] hover:border-[#1bac55]  hover:text-white mcra-button"
-                                    onClick={onClose}
-                                    radius="full"
-                                >
-                                    <span className="mcra-button-text">
-                                        Guardar
-                                    </span>
-                                </Button>
-                            </div>
-                        </ModalBody>
-                    </>
-                )}
-            </ModalContent>
-        </Modal>
+                                    {salas.map((sala) => (
+                                        <SelectItem key={sala.idSala}>
+                                            {sala.nombre}
+                                        </SelectItem>
+                                    ))}
+                                </Select>
+
+                                <div className="mcra-contenedor-botones">
+                                    <Button
+                                        className="bg-white border-4 border-[#ac3e1b] text-[#ac3e1b] hover:bg-[#ac3e1b] hover:border-[#ac3e1b] hover:text-white mcra-button"
+                                        onPress={onClose}
+                                        radius="full"
+                                    >
+                                        <span className="mcra-button-text">
+                                            Cancelar
+                                        </span>
+                                    </Button>
+                                    <Button
+                                        className="bg-white border-4 border-[#1bac55] text-[#1bac55] hover:bg-[#1bac55] hover:border-[#1bac55]  hover:text-white mcra-button"
+                                        onPress={handleAceptar}
+                                        radius="full"
+                                    >
+                                        <span className="mcra-button-text">
+                                            Aceptar
+                                        </span>
+                                    </Button>
+                                </div>
+                            </ModalBody>
+                        </>
+                    )}
+                </ModalContent>
+            </Modal>
+            <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+                <Alert
+                    data-cy="login-error"
+                    onClose={handleClose}
+                    severity="error"
+                    variant="filled"
+                    sx={{ width: "100%" }}
+                >
+                    {message}
+                </Alert>
+            </Snackbar>
+        </>
     );
 }
 
